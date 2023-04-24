9d560907e9cac24d225605f4f923d9bee1f947f9
AlexanderMar21 <54232340+AlexanderMar21@users.noreply.github.com>
2023-04-24 20:02:01 +0200

    feat: add amimation

--- src/components/ListWrapper.vue
+++ src/components/ListWrapper.vue
@@ -1,5 +1,6 @@
 <template>
 	<ol
+		v-auto-animate
 		class="block bg-white shadow-md rounded-2xl border max-h100 overflow-y-auto border-gray-300 max-h-340px"
 	>
 		<slot />
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import { createPinia } from 'pinia';
 import { VueFire, VueFireAuth } from 'vuefire';
 import 'virtual:windi.css';
 import { firebaseApp } from './services/firebase';
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
 
 const pinia = createPinia();
 
 const app = createApp(App);
 app.use(pinia);
+app.use(autoAnimatePlugin);
 app.use(VueFire, {
 	// imported above but could also just be created here
 	firebaseApp,
